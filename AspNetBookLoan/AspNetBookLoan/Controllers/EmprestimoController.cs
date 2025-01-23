4298c5bca0eb820803490760192e40005e6b239d
augustocsmiranda <augustocsmiranda@gmail.com>
2025-01-23 15:03:34 -0300

    Criação EmprestimoController, EmprestimosModel, Emprestimo Views, ApplicationDbContext, Create and Update Migration

---
+++ AspNetBookLoan/AspNetBookLoan/Controllers/EmprestimoController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetBookLoan.Data;
+using AspNetBookLoan.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace AspNetBookLoan.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
+        }
+    }
+}
---
+++ AspNetBookLoan/AspNetBookLoan/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AspNetBookLoan.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetBookLoan.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+
+
+    }
+}
